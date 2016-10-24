f5f96b664923a6c1669babf985d7819db593dd62
Rails Udemy Course <coolpaek@yahoo.com>
2016-10-24 23:13:08 +0000

    Created many-to-many relationships between user and stock

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :user_stocks
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
@@ -8,6 +9,8 @@ Rails.application.routes.draw do
   
   get 'my_portfolio', to:'users#my_portfolio'
   get 'search_stocks', to:'stocks#search'
+  
+  resources :user_stocks, except: [:show, :edit, :update]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
